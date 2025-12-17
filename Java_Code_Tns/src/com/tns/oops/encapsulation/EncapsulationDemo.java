99d538f96501f072a183d022390246ddd5e9bb63
APARNA SEENUVASAN <APARNA SEENUVASAN@LAPTOP-MCC35ITN>
2025-12-17 08:13:32 +0530

    Initial commit

---
+++ Java_Code_Tns/src/com/tns/oops/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,17 @@
+package com.tns.oops.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		OopsConceptDemo obj=new OopsConceptDemo();
+		obj.setSerialNum(110);
+		obj.setName("Aparna");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+
+	}
+
+}
